08c4f6766b5e0776ceacf6a16d20a0a381616cd0
kazuyuki <kazuyuki@kazuyuki-PC>
2017-03-23 17:14:20 +0900

    springboot

--- src/main/java/com/example/Practice314Application.java
+++ src/main/java/com/example/Practice314Application.java
@@ -2,15 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
 
 @SpringBootApplication
 public class Practice314Application {
 
-	public static void main(String[] args) {
-		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-		System.out.printf(new Pbkdf2PasswordEncoder().encode("demo"));
-		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+	public static void main(String[] args) {		
 		SpringApplication.run(Practice314Application.class, args);
 	}
 }
